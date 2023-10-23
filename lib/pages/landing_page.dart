e4a0ec8aeae11cafdbe8fac646fed3a5e2a174ca
DooxFonggg <phongcoi1235@gmail.com>
2023-10-23 22:29:08 +0700

    landing page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quizz_app/values/app_assets.dart';
 import 'package:flutter_quizz_app/values/app_colors.dart';
 import 'package:flutter_quizz_app/values/app_styles.dart';
 
@@ -24,12 +25,38 @@ class LandingPage extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                color: Colors.green,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'English',
+                      style: AppStyles.h2.copyWith(
+                          color: AppColor.blackGrey,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 36),
+                      child: Text(
+                        'Qoutes"',
+                        textAlign: TextAlign.right,
+                        style: AppStyles.h4.copyWith(height: 0.5),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
-              child: Container(
-                color: Colors.yellow,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 150),
+                child: RawMaterialButton(
+                  onPressed: () {
+                    // Xử lý khi nút được nhấn
+                  },
+                  child: Image.asset(AppAssest.rightArrow),
+                  fillColor: AppColor.lightBlue,
+                  shape: CircleBorder(),
+                ),
               ),
             ),
           ],
