d8d63fe26465d7dc5c6631822ffafd01efd294f8
DooxFonggg <phongcoi1235@gmail.com>
2023-10-24 22:10:10 +0700

    home  page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; // lay size ra
     return Scaffold(
       backgroundColor: AppColor.secondColor,
       appBar: AppBar(
@@ -28,6 +29,37 @@ class _HomePageState extends State<HomePage> {
           style: AppStyles.h3.copyWith(color: AppColor.textColor, fontSize: 36),
         ),
         centerTitle: true, // Đảm bảo tiêu đề được căn giữa theo chiều ngang
+      ),
+      body: Container(
+        width: double.infinity, // chiều rộng tối đa có thể
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Container(
+              height: size.height * 1 / 10,
+              alignment: Alignment.center,
+              child: Text(
+                '"It is amazing how complete is the delusion that beauty is goodness."',
+                style: AppStyles.h6
+                    .copyWith(fontSize: 12, color: AppColor.textColor),
+              ),
+            ),
+            Container(
+              height: size.height * 2 / 3,
+              child: PageView.builder(
+                  // tạo ra kiểu lướt list
+                  itemCount: 5, // số lần lướt
+                  itemBuilder: (context, index) {
+                    // index hiển thị số trang đã lướt đến
+                    return Container(
+                      decoration: BoxDecoration(
+                          color: AppColor.primaryColor,
+                          borderRadius: BorderRadius.all(Radius.circular(24))),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
